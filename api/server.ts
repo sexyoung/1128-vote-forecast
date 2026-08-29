1b1133298895310b27455a25d1df4964b2b5669e
sexyoung <Habuche@gmail.com>
2026-08-29 07:07:15 +0000

    ci: wire Vercel deployments

---
+++ api/server.ts
@@ -0,0 +1,21 @@
+import { app } from '../src/server/app.js';
+import { assertProductionEnv } from '../src/server/env.js';
+import { mountHtmlRoutes } from '../src/server/html.js';
+import { refreshCandidates } from '../src/server/prediction-targets.js';
+import { prodRenderer } from '../src/server/render-prod.js';
+
+assertProductionEnv();
+await refreshCandidates();
+mountHtmlRoutes(app, prodRenderer);
+
+export default {
+  fetch(request: Request) {
+    const url = new URL(request.url);
+    const path = url.searchParams.get('__vf_path');
+    if (!path) return app.fetch(request);
+
+    url.pathname = path;
+    url.searchParams.delete('__vf_path');
+    return app.fetch(new Request(url, request));
+  },
+};
